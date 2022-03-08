d69c2a59b5a3fbc055593c4274c77814a4a05474
eb18608 <eb18608@bristol.ac.uk>
2022-03-08 13:31:17 +0000

    Add discount object archetype

---
+++ src/com/main/java/Discount/DiscountObject.java
@@ -0,0 +1,21 @@
+package com.main.java.Discount;
+
+import com.main.java.Book;
+
+import java.util.HashSet;
+
+/**
+ * Discount class archetype. All will offer a %discount (float) to a applicable
+ * basket.
+ */
+
+public abstract class DiscountObject {
+
+    HashSet<Book> applicableBooks;
+    float percentageDiscount;
+
+    public DiscountObject (HashSet<Book> aB, float pD) {
+        this.applicableBooks = aB;
+        this.percentageDiscount = pD;
+    }
+}
